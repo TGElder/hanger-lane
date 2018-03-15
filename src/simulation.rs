0ad717310e858ddceeec44b93c8286b00ad704ab
Thomas Elder <tgelder@gmail.com>
2018-03-15 19:45:35 +0000

    Bad refactor attempt

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,35 +4,35 @@ extern crate network;
 use std::sync::mpsc::Receiver;
 use std::sync::Arc;
 use version::{Version, Publisher, Local};
-use super::{City, Traffic, Cell};
-use network::Network;
-use rand::Rng;
+use super::{City, Traffic, Cell, Direction};
+use network::{Network, Edge};
+use rand::{Rng, ThreadRng};
 
-pub enum SimulationMessage {
+pub enum SimulatorMessage {
     Start,
     Pause,
     Shutdown,
 }
 
-pub struct Simulation {
-    rx: Receiver<SimulationMessage>,
+pub struct Simulator {
+    rx: Receiver<SimulatorMessage>,
     city: Local<City>,
-    traffic: Traffic,
+    vehicles: usize,
     traffic_publisher: Publisher<Traffic>,
     running: bool,
     shutting_down: bool,
 }
 
-impl Simulation {
+impl Simulator {
 
-    pub fn new(rx: Receiver<SimulationMessage>,
+    pub fn new(rx: Receiver<SimulatorMessage>,
                city: &Version<City>,
                vehicles: usize,
-               traffic: &Version<Traffic>) -> Simulation {
-        Simulation{
+               traffic: &Version<Traffic>) -> Simulator {
+        Simulator{
             rx,
             city: Local::new(city),
-            traffic: Traffic::new(vehicles),
+            vehicles,
             traffic_publisher: Publisher::new(traffic),
             running: false,
             shutting_down: false,
@@ -41,8 +41,6 @@ impl Simulation {
 
 
     pub fn run(&mut self) {
-
-        let mut rng = rand::thread_rng();
 
         while !self.shutting_down {
 
@@ -59,66 +57,13 @@ impl Simulation {
             };
 
             if let Some(ref city) = city {
-                let target = city.get_index(&Cell{x: 256, y: 256, d: Direction::East});
-                let node_count = city.get_num_nodes();
-                println!("Edges");
                 let edges = city.create_edges();
-                println!("Network");
-                let network = Network::new(node_count, &edges);
-                println!("Dijkstra");
-                use super::Direction;
-                let costs = network.dijkstra(target);
-
-                let mut occupancy = vec![vec![false; city.width as usize]; city.height as usize];
-                clear_matrix(&mut occupancy);
-
-                println!("Setting occupancy");
-                for vehicle in self.traffic.vehicles.iter_mut() {
-                    *occupancy.get_mut(vehicle.x as usize).unwrap().get_mut(vehicle.y as usize).unwrap() = true;
-                }
+                let mut simulation = Simulation::new(city, &edges, &self.vehicles);
 
                 while self.running {
-                    println!("Simulating traffic with city version {}", city.id);
-                    println!("Clearing occupancy");
-
-                    for vehicle in self.traffic.vehicles.iter_mut() {
-                        // Find node that vehicle occupies
-                        let node = city.get_index(vehicle);
-                        // Find adjacent nodes (easy using network)
-                        let neighbours: Vec<u32> = network.get_out(node).iter().map(|e| e.to).collect();
-                        // Filter this to free nodes
-                        let free_neighbours: Vec<u32> = neighbours.iter().cloned()
-                            .filter(|n| {
-                                let cell = city.get_cell(*n);
-                                !occupancy.get(cell.x as usize).unwrap().get(cell.y as usize).unwrap()
-                            }).collect();
-                        // Get lowest cost of neighbour
-                        let lowest_cost = free_neighbours.iter()
-                            .map(|n| costs.get(*n as usize))
-                            .min();
-                        if let Some(lowest_cost) = lowest_cost {
-                            if lowest_cost < costs.get(node as usize) {
-                                // Get some neighbour with lowest cost
-                                let candidates: Vec<u32> = free_neighbours.iter().cloned()
-                                    .filter(|n| costs.get(*n as usize) == lowest_cost)
-                                    .collect();
-                                let selected = rng.choose(&candidates).unwrap();
-                                
-                                *occupancy.get_mut(vehicle.x as usize).unwrap().get_mut(vehicle.y as usize).unwrap() = false;
-                                let cell = city.get_cell(*selected);
-                                vehicle.x = cell.x;
-                                vehicle.y = cell.y;
-                                vehicle.d = cell.d;
-                                    
-                                if (*selected != target) {
-                                    *occupancy.get_mut(vehicle.x as usize).unwrap().get_mut(vehicle.y as usize).unwrap() = true;
-                                }
-                            }
-                        }
-                    }
-                    self.traffic.id += 1;
-                    self.traffic_publisher.publish(&self.traffic);
+                    simulation.step();
                     self.check_messages();
+                    self.traffic_publisher.publish(&simulation.traffic);
                 }
 
             }
@@ -136,15 +81,15 @@ impl Simulation {
         match self.rx.try_recv() {
             Ok(m) => {
                 match m {
-                    SimulationMessage::Start => {
+                    SimulatorMessage::Start => {
                         println!("Starting simulation");
                         self.running = true;
                     },
-                    SimulationMessage::Pause => {
+                    SimulatorMessage::Pause => {
                         println!("Pausing simulation");
                         self.running = false;
                     },
-                    SimulationMessage::Shutdown => {
+                    SimulatorMessage::Shutdown => {
                         println!("Shutting down simulation");
                         self.running = false;
                         self.shutting_down = true;
@@ -158,15 +103,109 @@ impl Simulation {
 
 }
 
+struct Simulation<'a> {
+    rng: ThreadRng,
+    traffic: Traffic,
+    city: &'a Arc<City>,
+    target: u32,
+    node_count: u32,
+    network: Network<'a>,
+    costs: Vec<Option<u32>>,
+    occupancy: Occupancy,
+}
 
+impl <'a> Simulation<'a> {
 
-fn clear_matrix(matrix: &mut Vec<Vec<bool>>) {
-    for row in matrix {
-        for cell in row {
-            *cell = false;
+    fn new(city: &'a Arc<City>, edges: &'a Vec<Edge>, vehicles: &usize) -> Simulation<'a> {
+        let target = city.get_index(&Cell{x: 256, y: 256, d: Direction::East});
+        let node_count = city.get_num_nodes();
+        let network = Network::new(node_count, edges);
+        let costs = network.dijkstra(target);
+        let traffic = Traffic::new(*vehicles);
+        let mut occupancy = Occupancy::new(city, &traffic.vehicles);
+        Simulation {
+            rng: rand::thread_rng(),
+            traffic,
+            city,
+            target,
+            node_count,
+            network,
+            costs,
+            occupancy
         }
     }
 
+    fn step(&mut self) {
+        println!("Simulating traffic with city version {}", self.city.id);
+        println!("Clearing occupancy");
+
+        for vehicle in self.traffic.vehicles.iter_mut() {
+            let node = self.city.get_index(vehicle);
+            let neighbours: Vec<u32> = self.network.get_out(node).iter().map(|e| e.to).collect();
+            let free_neighbours: Vec<u32> = neighbours.iter().cloned()
+                .filter(|n| {
+                    let cell = self.city.get_cell(*n);
+                    !self.occupancy.is_free(cell.x as usize, cell.y as usize)
+                }).collect();
+            let lowest_cost = free_neighbours.iter()
+                .map(|n| self.costs.get(*n as usize))
+                .min();
+            if let Some(lowest_cost) = lowest_cost {
+                if lowest_cost < self.costs.get(node as usize) {
+                    // Get some neighbour with lowest cost
+                    let candidates: Vec<u32> = free_neighbours.iter().cloned()
+                        .filter(|n| self.costs.get(*n as usize) == lowest_cost)
+                        .collect();
+                    let selected = self.rng.choose(&candidates).unwrap();
+                    
+                    self.occupancy.free(vehicle);
+                    let cell = self.city.get_cell(*selected);
+                    vehicle.x = cell.x;
+                    vehicle.y = cell.y;
+                    vehicle.d = cell.d;
+                        
+                    if *selected != self.target {
+                        self.occupancy.occupy(vehicle);
+                    }
+                }
+            }
+        }
+        self.traffic.id += 1;
+    }
 
 }
+
+struct Occupancy {
+    occupancy: Vec<Vec<bool>>,
+}
+
+impl Occupancy {
+
+    fn new(city: &City, vehicles: &Vec<Cell>) -> Occupancy {
+        let occupancy = vec![vec![false; city.width as usize]; city.height as usize];
+        let mut out = Occupancy{ occupancy };
+        for vehicle in vehicles.iter() {
+            out.occupy(vehicle);
+        }
+        out
+    }
+    
+    fn is_free(&self, x: usize, y: usize) -> &bool {
+        self.occupancy.get(x).unwrap().get(y).unwrap()
+    }
+
+    fn set(&mut self, vehicle: &Cell, value: bool) {
+        *self.occupancy.get_mut(vehicle.x as usize).unwrap().get_mut(vehicle.y as usize).unwrap() = value;
+    }
+
+    fn free(&mut self, vehicle: &Cell) {
+        self.set(vehicle, false);
+    }
+
+    fn occupy(&mut self, vehicle: &Cell) {
+        self.set(vehicle, true);
+    }
+
+}
+
 
--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, RwLock, mpsc};
 use std::sync::mpsc::Sender;
 use std::time::Duration;
 use version::Publisher;
-use simulation::{Simulation, SimulationMessage};
+use simulation::{Simulator, SimulatorMessage};
 use super::City;
 use graphics::Graphics;
 use editor::Editor;
@@ -21,7 +21,7 @@ impl UI {
         let traffic = Arc::new(RwLock::new(None));
 
         let (sim_tx, sim_rx) = mpsc::channel();
-        let mut sim = Simulation::new(sim_rx, &city, 8192, &traffic);
+        let mut sim = Simulator::new(sim_rx, &city, 8192, &traffic);
         let mut graphics = Graphics::new(&city, &traffic, "Hanger Lane", 1024, 1024);
         let mut editor = Editor::new(&city);
 
@@ -32,18 +32,18 @@ impl UI {
         let sim_tx_2 = Sender::clone(&sim_tx);
         thread::spawn(move || {
             thread::sleep(Duration::from_secs(1));
-            sim_tx_2.send(SimulationMessage::Start).unwrap();
+            sim_tx_2.send(SimulatorMessage::Start).unwrap();
 
             thread::sleep(Duration::from_secs(30));
             editor.run();
 
             thread::sleep(Duration::from_secs(1));
-            sim_tx_2.send(SimulationMessage::Pause).unwrap();
+            sim_tx_2.send(SimulatorMessage::Pause).unwrap();
         });
 
         // Window needs to be created in main thread
         graphics.run();
-        sim_tx.send(SimulationMessage::Shutdown).unwrap();
+        sim_tx.send(SimulatorMessage::Shutdown).unwrap();
 
         sim_handle.join().unwrap();
 
